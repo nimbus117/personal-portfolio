d1566693dde43131cee4686d743faa0b5da775ce
nimbus117 <mail@nimbus117.co.uk>
2018-04-29 18:33:23 +0100

    created a template using string literals for the modal content

--- js/index.js
+++ js/index.js
@@ -1,3 +1,33 @@
-$(document).ready(function(){
+$(document).ready(function(){  
   $('body').scrollspy({ target: '.navbar'})
+  var markup = "";
+  for (var x = 0 ; x < modalData.length ; x++) {
+    markup += `
+    <div class="col-sm-6 col-lg-4">
+      <a data-toggle="modal" href="#portfolioModal${x}">
+        <img class="img-fluid img-thumbnail" src="${modalData[x].image}" alt="${modalData[x].alt}" title="${modalData[x].title}">
+      </a>
+      <div id="portfolioModal${x}" class="modal fade">
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title">${modalData[x].title}</h5>
+              <button type="button" class="close" data-dismiss="modal">
+              <span>&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">
+              <img class="img-fluid img-thumbnail" src="${modalData[x].image}" alt="${modalData[x].alt}" title="${modalData[x].alt}">
+              <p><strong>${modalData[x].date}</strong> - ${modalData[x].caption}</p>
+            </div>
+            <div class="modal-footer">
+              <a role="button" class="btn btn-dark" href="${modalData[x].link}" target="_blank">Visit Page</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    `;
+  }
+  document.getElementById("modal-row").innerHTML = markup;
 });
---
+++ js/modalData.js
@@ -0,0 +1,92 @@
+/*
+  {
+    "title": "",
+    "date": "",
+    "image": "",
+    "alt": "",
+    "caption": "",
+    "link": ""
+  },
+*/
+var modalData = [
+  {
+    "title": "Show the local weather",
+    "date": "26/04/2018",
+    "image": "/images/fcc-local-weather.png",
+    "alt": "freeCodeCamp local weather",
+    "caption": "Second of the<a href='https://www.freecodecamp.org/challenges/show-the-local-weather' target='_blank'> freeCodeCamp </a>intermediate front end development projects.",
+    "link": "https://codepen.io/nimbus117/full/MGebmP/"
+  },
+  {
+    "title": "Build a Random Quote Machine",
+    "date": "24/04/2018",
+    "image": "/images/fcc-quote-machine.png",
+    "alt": "freeCodeCamp quote machine",
+    "caption": "First of the<a href='https://www.freecodecamp.org/challenges/build-a-random-quote-machine' target='_blank'> freeCodeCamp </a>intermediate front end development projects.",
+    "link": "https://codepen.io/nimbus117/full/LmNyZV/"
+  },
+  {
+    "title": "Build a Personal Portfolio Webpage",
+    "date": "06/04/2018",
+    "image": "/images/fcc-personal-portfolio.png",
+    "alt": "freeCodeCamp personal portfolio",
+    "caption": "The second<a href='https://www.freecodecamp.org/challenges/build-a-personal-portfolio-webpage' target='_blank'> freeCodeCamp </a>basic front end development project. Started from scratch this time after watching<a href='https://scrimba.com/g/gbootstrap4' target='_blank'> these </a>awesome videos on bootstrap.",
+    "link": "https://codepen.io/nimbus117/full/zWmqNj/"
+  },
+  {
+    "title": "Build a Tribute Page",
+    "date": "05/04/2018",
+    "image": "/images/fcc-tribute-page.png",
+    "alt": "freeCodeCamp tribute page",
+    "caption": "My take on the first<a href='https://www.freecodecamp.org/challenges/build-a-tribute-page' target='_blank'> freeCodeCamp </a>basic front end development project. I didn't really know what I was doing here so built a copy of the example to understand how it was done then added a carousel for the images and some buttons to change the theme.",
+    "link": "https://codepen.io/nimbus117/full/bvxgmO/"
+  },
+  {
+    "title": "zWindowsUpdate PowerShell Module",
+    "date": "29/03/2018",
+    "image": "/images/psgallery.png",
+    "alt": "powershell module",
+    "caption": "This is a PowerShell module I wrote and published on the PowerShell Gallery. It's main use is to run windows update on multiple remote computers in a controlled fashion. Anything to speed up manually patching at 3:00am.",
+    "link": "https://www.powershellgallery.com/packages/zWindowsUpdate/"
+  },
+  {
+    "title": "Old Blog",
+    "date": "06/07/2015",
+    "image": "/images/oldblog.png",
+    "alt": "my old blog",
+    "caption": "My first go at building a blog a few years ago. I built it using<a href='https://jekyllrb.com/' target='_blank'> jekyll </a>and like this page it's hosted in AWS S3.",
+    "link": "/old-blog"
+  },
+  {
+    "title": "Placeholder",
+    "date": "01/01/1970",
+    "image": "/images/placeholder.png",
+    "alt": "placeholder-image",
+    "caption": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation",
+    "link": "https://www.google.com"
+  },
+  {
+    "title": "Placeholder",
+    "date": "01/01/1970",
+    "image": "/images/placeholder.png",
+    "alt": "placeholder-image",
+    "caption": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation",
+    "link": "https://www.google.com"
+  },
+  {
+    "title": "Placeholder",
+    "date": "01/01/1970",
+    "image": "/images/placeholder.png",
+    "alt": "placeholder-image",
+    "caption": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation",
+    "link": "https://www.google.com"
+  },
+  {
+    "title": "Placeholder",
+    "date": "01/01/1970",
+    "image": "/images/placeholder.png",
+    "alt": "placeholder-image",
+    "caption": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation",
+    "link": "https://www.google.com"
+  }
+];
