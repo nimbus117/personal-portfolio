a17f77e6d8066e1260b161a843e18f4731b48a71
nimbus117 <mail@nimbus117.co.uk>
2018-05-08 21:08:09 +0100

    changed to using css variables and added guid to portData

--- js/main.js
+++ js/main.js
@@ -3,14 +3,14 @@ var portCards = "";
 for (var x = 0; x < portData.length; x++) {
   portCards += `
   <div class="col-sm-6 col-lg-4">
-    <div class="card n-d1-b">
-      <div class="img-thumbnail n-d1-b">
+    <div class="card">
+      <div class="img-thumbnail">
         <a id="card-image" data-toggle="modal" href="#portModal${x}">
           <img class="img-fluid" 
                src="${portData[x].image}" 
                alt="${portData[x].alt}">
         </a>
-        <p id="card-caption" class="text-center text-truncate n-l2-t">
+        <p id="card-caption" class="text-center text-truncate">
           ${portData[x].title}
         </p>
       </div>
@@ -18,18 +18,18 @@ for (var x = 0; x < portData.length; x++) {
     <div id="portModal${x}" class="modal fade">
       <div class="modal-dialog">
         <div class="modal-content">
-          <div class="modal-header n-d1-b">
-            <h5 class="modal-title n-l2-t">${portData[x].title}</h5>
+          <div class="modal-header">
+            <h5 class="modal-title">${portData[x].title}</h5>
             <button type="button" class="close" data-dismiss="modal">
-            <span class="n-l2-t">&times;</span>
+            <span>&times;</span>
             </button>
           </div>
           <div class="modal-body">
-            <img id="modal-image" class="img-thumbnail n-d1-b" 
+            <img id="modal-image" class="img-thumbnail" 
                  src="${portData[x].image}" 
                  alt="${portData[x].alt}">
             <p><strong>${portData[x].date}</strong> - ${portData[x].caption}</p>
-            <a role="button" class="btn btn-outline-info btn-block" 
+            <a role="button" class="my-btn" 
                href="${portData[x].link}" 
                target="_blank">Visit Page</a>
           </div>
